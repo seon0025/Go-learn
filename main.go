1eaf72a1bf35f4fd7cb90ed50d83db1633859791
선진 <s22013@gsm.hs.kr>
2023-07-06 23:24:46 +0900

    Dictionary

--- main.go
+++ main.go
@@ -3,11 +3,15 @@ package main
 import (
 	"fmt"
 
-	"github.com/seon0025/learngo/accounts"
+	"github.com/seon0025/learngo/mydict"
 )
 
 func main() {
-	account := accounts.NewAccount("seon")
-	account.Deposit(10)
-	fmt.Println(account)
+	dictionary := mydict.Dictionary{"first": "firstword"}
+	definition, error := dictionary.Search("second")
+	if error != nil {
+		fmt.Println(error)
+	} else {
+		fmt.Println(definition)
+	}
 }
