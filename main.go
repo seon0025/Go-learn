7ea59bff2fad9fbc81c47d2249c6fb6bfcab7343
선진 <s22013@gsm.hs.kr>
2023-07-12 18:21:50 +0900

    hitURL

--- main.go
+++ main.go
@@ -1,13 +1,31 @@
 package main
 
-import "fmt"
+import (
+	"errors"
+	"fmt"
+	"net/http"
+)
+
+var errRequestFailed = errors.New("request failed")
 
 func main() {
 	urls := []string{
 		"https://www.google.com/",
+		"https://www.naver.com/",
+		"https://www.daum.net/",
 	}
 
-	for url := range urls {
-		fmt.Println(url)
+	for _, url := range urls {
+		hitURL(url)
+
 	}
+}
+
+func hitURL(url string) error {
+	fmt.Println("checking:", url)
+	resp, err := http.Get(url)
+	if err == nil || resp.StatusCode >= 400 {
+		return errRequestFailed
+	}
+	return nil
 }
