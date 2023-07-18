3c8751466fd52b0629ab26f5aca0496fa539bdd1
선진 <s22013@gsm.hs.kr>
2023-07-18 14:58:54 +0900

    extractJob

--- main.go
+++ main.go
@@ -5,21 +5,38 @@ import (
 	"log"
 	"net/http"
 	"strconv"
+	"strings"
 
 	"github.com/PuerkitoBio/goquery"
 )
 
+type extractedJob struct {
+	id       string
+	title    string
+	location string
+	date     string
+	company  string
+}
+
+type jobResult struct {
+	
+}
+
 var baseURL string = "https://www.saramin.co.kr/zf_user/search/recruit?&searchword=python"
 
 func main() {
+	var jobs []extractedJob
 	totalPages := getPages()
 	for i := 0; i < totalPages; i++ {
-		getPage(i)
+		extractedJob := getPage(i)
+		jobs = append(jobs, extractedJob...)
 	}
+	fmt.Println(jobs)
 }
 
-func getPage(page int) {
-	pageURL := baseURL + "&start=" + strconv.Itoa(page*50)
+func getPage(page int) []extractedJob {
+	var jobs []extractedJob
+	pageURL := baseURL + "&recruitPage=" + strconv.Itoa(page) + "&recruitSort=relation&recruitPageCount=40&inner_com_type=&company_cd=0%2C1%2C2%2C3%2C4%2C5%2C6%2C7%2C9%2C10&show_applied=&quick_apply=&except_read=&ai_head_hunting=&mainSearch=n"
 	fmt.Println("requesting: ", pageURL)
 	res, err := http.Get(pageURL)
 	checkErr(err)
@@ -28,10 +45,24 @@ func getPage(page int) {
 	doc, err := goquery.NewDocumentFromReader(res.Body)
 	checkErr(err)
 	searchCards := doc.Find(".item_recruit")
-	searchCards.Each(func(i int, s *goquery.Selection){
-		id, _ := s.Attr("value")
-		fmt.Println(id)
+	searchCards.Each(func(i int, s *goquery.Selection) {
+		job := extracJob(s)
+		jobs = append(jobs, job)
 	})
+	return jobs
+}
+
+func extracJob(s *goquery.Selection) extractedJob {
+	id, _ := s.Attr("value")
+	title := cleanString(s.Find(".job_tit>a").Text())
+	location := cleanString(s.Find(".job_condition>span>a").Text())
+	company := cleanString(s.Find(".corp_name>a").Text())
+	date := cleanString(s.Find(".job_date>span").Text())
+	return extractedJob{id: id, title: title, location: location, company: company, date: date}
+}
+
+func cleanString(str string) string {
+	return strings.Join(strings.Fields(strings.TrimSpace(str)), " ")
 }
 
 func getPages() int {
